975e9540371afc59f0718eff92d6f0b481e91412
Mike G <mik8980@gmail.com>
2022-02-15 23:10:26 +0200

    исправил

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CommandHandler
 def greet_user(update, context):
-    print('Вызван /start :)')
+    print('Вызван /start ')
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start ')
 
 def main():

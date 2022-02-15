4635dd7effe110aaa4f9bf6c82af8119c397d8de
Mike G <mik8980@gmail.com>
2022-02-15 23:01:16 +0200

    [eqyz]

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from telegram.ext import Updater, CommandHandler
 def greet_user(update, context):
     print('Вызван /start')
-    
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start ')
+
 def main():
 
     mybot = Updater("5151686095:AAEOQ9HpV40J2RwZ0Vfs_mnRachGLNE2n2Q", use_context = True)

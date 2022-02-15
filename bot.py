0e2b67952cfdc5ce24d404321da67b7a3193bbc3
Mike G <mik8980@gmail.com>
2022-02-15 22:41:45 +0200

    add print start

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
-from telegram.ext import Updater
-
+from telegram.ext import Updater, CommandHandler
+def greet_user(update, context):
+    print('Вызван /start')
+    
 def main():
 
     mybot = Updater("5151686095:AAEOQ9HpV40J2RwZ0Vfs_mnRachGLNE2n2Q", use_context = True)
+   
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+   
     mybot.start_polling()
     mybot.idle()
 
